26adfc142fc06a468182e865b04b17bdd775b326
Egor Malyshev <egor.malyshev@umbrellait.com>
2022-02-05 17:58:10 +0300

    Added project and gitignore

---
+++ MyFinances/MyFinancesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFinancesApp.swift
+//  MyFinances
+//
+//  Created by Egor Malyshev on 05.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyFinancesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
